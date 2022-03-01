520bb49ec115acaabd42a9b1fe8b305cc43cd6ff
sedat <sedatcyalcin@gmail.com>
2022-03-01 17:07:43 +0300

    add lua nvim config

---
+++ nvim-lua/init.lua
@@ -0,0 +1,15 @@
+-----------------------------------------------------------
+-- Import Lua modules
+-----------------------------------------------------------
+require('settings')
+require('keymaps')
+require('plugins/packer')
+require('plugins/nvim-tree')
+require('plugins/indent-blankline')
+require('plugins/feline')
+require('plugins/vista')
+require('plugins/nvim-cmp')
+require('plugins/nvim-lspconfig')
+require('plugins/nvim-treesitter')
+require('plugins/alpha-nvim')
+require('plugins/nvim-web-devicons')
